e0720b7b5351f9c399745d2577cbcd44940bf092
japssii <japspagdilao@gmail.com>
2022-02-18 00:33:21 +0800

    Type update

---
+++ app/src/main/java/com/example/kamay/DashboardType.java
@@ -0,0 +1,34 @@
+package com.example.kamay;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DashboardType extends AppCompatActivity {
+  EditText editName;
+ Button buttonClick;
+   TextView textName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editName=(EditText) findViewById(R.id.editName);
+        buttonClick=(Button) findViewById(R.id.buttonClick);
+        textName =(TextView) findViewById(R.id.textName);
+
+        buttonClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                String name=editName.getText().toString();
+                textName.setText(""+name);
+            }
+        });
+
+    }
+
+}
