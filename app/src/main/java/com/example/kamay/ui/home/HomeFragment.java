a5206d493dca02ef93abb4417c42137443062005
QVidad <qvidad@gmail.com>
2022-03-08 18:37:03 +0800

    [New] Hand Recognition

--- app/src/main/java/com/example/kamay/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/kamay/ui/home/HomeFragment.java
@@ -1,15 +1,11 @@
 package com.example.kamay.ui.home;
 
 import android.Manifest;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
@@ -17,73 +13,65 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.chaquo.python.PyObject;
-import com.chaquo.python.Python;
-import com.chaquo.python.android.AndroidPlatform;
+import com.example.kamay.HandsResultGlRenderer;
+import com.example.kamay.HandsResultImageView;
 import com.example.kamay.R;
 import com.example.kamay.databinding.FragmentHomeBinding;
 
-import android.content.DialogInterface;
-import android.content.res.Resources;
+import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
+import android.graphics.Matrix;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.text.method.ScrollingMovementMethod;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Gravity;
-import android.view.SurfaceView;
 import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
-import android.widget.ImageButton;
-import android.widget.LinearLayout;
-import android.widget.Scroller;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.exifinterface.media.ExifInterface;
+// ContentResolver dependency
+import com.google.mediapipe.formats.proto.LandmarkProto.Landmark;
+import com.google.mediapipe.formats.proto.LandmarkProto.NormalizedLandmark;
+import com.google.mediapipe.solutioncore.CameraInput;
+import com.google.mediapipe.solutioncore.SolutionGlSurfaceView;
+import com.google.mediapipe.solutioncore.VideoInput;
+import com.google.mediapipe.solutions.hands.HandLandmark;
+import com.google.mediapipe.solutions.hands.Hands;
+import com.google.mediapipe.solutions.hands.HandsOptions;
+import com.google.mediapipe.solutions.hands.HandsResult;
+import java.io.IOException;
+import java.io.InputStream;
 
-import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.JavaCameraView;
-import org.opencv.android.LoaderCallbackInterface;
-import org.opencv.android.OpenCVLoader;
-import org.opencv.android.Utils;
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Scalar;
-import org.opencv.imgproc.Imgproc;
-
-public class HomeFragment extends Fragment implements CameraBridgeViewBase.CvCameraViewListener2{
+public class HomeFragment extends Fragment {
     private static String TAG = "HomeFragment" ;
-    private Mat mRgba,mGray;
-    private CameraBridgeViewBase mOpenCvCameraView;
-
     private FragmentHomeBinding binding;
     private int CAMERA_CODE = 100;
 
-    private BaseLoaderCallback mLoaderCallback =new BaseLoaderCallback(this.getActivity()) {
-        @Override
-        public void onManagerConnected(int status) {
-            switch (status){
-                case LoaderCallbackInterface
-                        .SUCCESS:{
-                    Log.i(TAG,"OpenCv Is loaded");
-                    mOpenCvCameraView.enableView();
-                }
-                default:
-                {
-                    super.onManagerConnected(status);
+    private Hands hands;
+    // Run the pipeline and the model inference on GPU or CPU.
+    private static final boolean RUN_ON_GPU = true;
+    private enum InputSource {
+        UNKNOWN,
+        IMAGE,
+        VIDEO,
+        CAMERA,
+    }
+    private InputSource inputSource = InputSource.UNKNOWN;
 
-                }
-                break;
-            }
-        }
-    };
+    // Image demo UI and image loader components.
+    private HandsResultImageView imageView;
+    // Video demo UI and video loader components.
+    private VideoInput videoInput;
+    private ActivityResultLauncher<Intent> videoGetter;
+    // Live camera demo UI and camera components.
+    private CameraInput cameraInput;
+
+    private SolutionGlSurfaceView<HandsResult> glSurfaceView;
+
     public HomeFragment(){
         Log.i(TAG,"Instantiated new "+this.getClass());
     }
@@ -96,25 +84,12 @@ public class HomeFragment extends Fragment implements CameraBridgeViewBase.CvCam
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        /*if(!Python.isStarted()){
-            Python.start(new AndroidPlatform(getContext()));
-        }
-        Python py = Python.getInstance();
-        PyObject pyobj =py.getModule("script");
-        PyObject obj = pyobj.callAttr("main");
-
-        binding.translatedText.setText(obj.toString());*/
-        binding.translatedText.setText("Hello from Javascript");
+        imageView = new HandsResultImageView(this.getContext());
+        setupLiveDemoUiComponents();
 
         if (ContextCompat.checkSelfPermission(HomeFragment.this.getContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
             requestPermissions(new String[]{Manifest.permission.CAMERA}, CAMERA_CODE);
         }
-
-        mOpenCvCameraView=(CameraBridgeViewBase) root.findViewById(R.id.frame_Surface);
-        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
-        mOpenCvCameraView.setCvCameraViewListener(HomeFragment.this);
-        
-
         return root;
     }
 
@@ -129,55 +104,186 @@ public class HomeFragment extends Fragment implements CameraBridgeViewBase.CvCam
             }
         }
     }
-
-    @Override
-    public void onCameraViewStarted(int width, int height) {
-        mRgba=new Mat(height,width, CvType.CV_8UC4);
-        mGray =new Mat(height,width,CvType.CV_8UC1);
-    }
-
-    @Override
-    public void onCameraViewStopped() {
-        mRgba.release();
-    }
-
-    @Override
-    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        mRgba=inputFrame.rgba();
-        mGray=inputFrame.gray();
-
-        return mRgba;
-    }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
-        if (mOpenCvCameraView != null) {
-            mOpenCvCameraView.disableView();
-        }
     }
-
     @Override
     public void onPause() {
         super.onPause();
-        if (mOpenCvCameraView !=null){
-            mOpenCvCameraView.disableView();
+        if (inputSource == InputSource.CAMERA) {
+            glSurfaceView.setVisibility(View.GONE);
+            cameraInput.close();
+        } else if (inputSource == InputSource.VIDEO) {
+            videoInput.pause();
         }
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        if (OpenCVLoader.initDebug()){
-            //if load success
-            Log.d(TAG,"Opencv initialization is done");
-            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        if (inputSource == InputSource.CAMERA) {
+            // Restarts the camera and the opengl surface rendering.
+            cameraInput = new CameraInput(this.getActivity());
+            cameraInput.setNewFrameListener(textureFrame -> hands.send(textureFrame));
+            glSurfaceView.post(this::startCamera);
+            glSurfaceView.setVisibility(View.VISIBLE);
+        } else if (inputSource == InputSource.VIDEO) {
+            videoInput.resume();
         }
-        else{
-            //if not loaded
-            Log.d(TAG,"Opencv is not loaded. try again");
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_4_0,this.getActivity(),mLoaderCallback);
+    }
+    private Bitmap rotateBitmap(Bitmap inputBitmap, InputStream imageData) throws IOException {
+        int orientation =
+                new ExifInterface(imageData)
+                        .getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+        if (orientation == ExifInterface.ORIENTATION_NORMAL) {
+            return inputBitmap;
+        }
+        Matrix matrix = new Matrix();
+        switch (orientation) {
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                matrix.postRotate(90);
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                matrix.postRotate(180);
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                matrix.postRotate(270);
+                break;
+            default:
+                matrix.postRotate(0);
+        }
+        return Bitmap.createBitmap(
+                inputBitmap, 0, 0, inputBitmap.getWidth(), inputBitmap.getHeight(), matrix, true);
+    }
+
+    private Bitmap downscaleBitmap(Bitmap originalBitmap) {
+        double aspectRatio = (double) originalBitmap.getWidth() / originalBitmap.getHeight();
+        int width = imageView.getWidth();
+        int height = imageView.getHeight();
+        if (((double) imageView.getWidth() / imageView.getHeight()) > aspectRatio) {
+            width = (int) (height * aspectRatio);
+        } else {
+            height = (int) (width / aspectRatio);
+        }
+        return Bitmap.createScaledBitmap(originalBitmap, width, height, false);
+    }
+
+
+    /** Sets up the UI components for the live demo with camera input. */
+    private void setupLiveDemoUiComponents() {
+        stopCurrentPipeline();
+        setupStreamingModePipeline(InputSource.CAMERA);
+    }
+
+    /** Sets up core workflow for streaming mode. */
+    private void setupStreamingModePipeline(InputSource inputSource) {
+        this.inputSource = inputSource;
+        // Initializes a new MediaPipe Hands solution instance in the streaming mode.
+        hands =
+                new Hands(
+                        this.getContext(),
+                        HandsOptions.builder()
+                                .setStaticImageMode(false)
+                                .setMaxNumHands(2)
+                                .setRunOnGpu(RUN_ON_GPU)
+                                .build());
+        hands.setErrorListener((message, e) -> Log.e(TAG, "MediaPipe Hands error:" + message));
+
+        if (inputSource == InputSource.CAMERA) {
+            cameraInput = new CameraInput(this.getActivity());
+            cameraInput.setNewFrameListener(textureFrame -> hands.send(textureFrame));
+        } else if (inputSource == InputSource.VIDEO) {
+            videoInput = new VideoInput(this.getActivity());
+            videoInput.setNewFrameListener(textureFrame -> hands.send(textureFrame));
+        }
+
+        // Initializes a new Gl surface view with a user-defined HandsResultGlRenderer.
+        glSurfaceView =
+                new SolutionGlSurfaceView(this.getContext(), hands.getGlContext(), hands.getGlMajorVersion());
+        glSurfaceView.setSolutionResultRenderer(new HandsResultGlRenderer());
+        glSurfaceView.setRenderInputImage(true);
+        hands.setResultListener(
+                handsResult -> {
+                    logWristLandmark(handsResult, /*showPixelValues=*/ false);
+                    glSurfaceView.setRenderData(handsResult);
+                    glSurfaceView.requestRender();
+                });
+
+        // The runnable to start camera after the gl surface view is attached.
+        // For video input source, videoInput.start() will be called when the video uri is available.
+        if (inputSource == InputSource.CAMERA) {
+            glSurfaceView.post(this::startCamera);
+        }
+
+        // Updates the preview layout.
+        FrameLayout frameLayout = binding.previewDisplayLayout;
+        imageView.setVisibility(View.GONE);
+        frameLayout.removeAllViewsInLayout();
+        frameLayout.addView(glSurfaceView);
+        glSurfaceView.setVisibility(View.VISIBLE);
+        frameLayout.requestLayout();
+    }
+
+    private void startCamera() {
+        cameraInput.start(
+                this.getActivity(),
+                hands.getGlContext(),
+                CameraInput.CameraFacing.BACK,
+                glSurfaceView.getWidth(),
+                glSurfaceView.getHeight());
+    }
+
+    private void stopCurrentPipeline() {
+        if (cameraInput != null) {
+            cameraInput.setNewFrameListener(null);
+            cameraInput.close();
+        }
+        if (videoInput != null) {
+            videoInput.setNewFrameListener(null);
+            videoInput.close();
+        }
+        if (glSurfaceView != null) {
+            glSurfaceView.setVisibility(View.GONE);
+        }
+        if (hands != null) {
+            hands.close();
         }
     }
 
+    private void logWristLandmark(HandsResult result, boolean showPixelValues) {
+        if (result.multiHandLandmarks().isEmpty()) {
+            return;
+        }
+        NormalizedLandmark wristLandmark =
+                result.multiHandLandmarks().get(0).getLandmarkList().get(HandLandmark.WRIST);
+        // For Bitmaps, show the pixel values. For texture inputs, show the normalized coordinates.
+        if (showPixelValues) {
+            int width = result.inputBitmap().getWidth();
+            int height = result.inputBitmap().getHeight();
+            Log.i(
+                    TAG,
+                    String.format(
+                            "MediaPipe Hand wrist coordinates (pixel values): x=%f, y=%f",
+                            wristLandmark.getX() * width, wristLandmark.getY() * height));
+        } else {
+            Log.i(
+                    TAG,
+                    String.format(
+                            "MediaPipe Hand wrist normalized coordinates (value range: [0, 1]): x=%f, y=%f",
+                            wristLandmark.getX(), wristLandmark.getY()));
+        }
+        if (result.multiHandWorldLandmarks().isEmpty()) {
+            return;
+        }
+        Landmark wristWorldLandmark =
+                result.multiHandWorldLandmarks().get(0).getLandmarkList().get(HandLandmark.WRIST);
+        Log.i(
+                TAG,
+                String.format(
+                        "MediaPipe Hand wrist world coordinates (in meters with the origin at the hand's"
+                                + " approximate geometric center): x=%f m, y=%f m, z=%f m",
+                        wristWorldLandmark.getX(), wristWorldLandmark.getY(), wristWorldLandmark.getZ()));
+    }
 }
