d0fa2e2aab8a586a7b55915cac9db37c897a1b3f
QVidad <qvidad@gmail.com>
2022-03-10 03:41:13 +0800

    [Update] Denied Camera Permission and Cleaning Home Fragment

--- app/src/main/java/com/example/kamay/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/kamay/ui/home/HomeFragment.java
@@ -5,46 +5,34 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.kamay.HandsResultGlRenderer;
 import com.example.kamay.HandsResultImageView;
-import com.example.kamay.R;
 import com.example.kamay.databinding.FragmentHomeBinding;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.Matrix;
-import android.os.Bundle;
-import android.provider.MediaStore;
-import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.exifinterface.media.ExifInterface;
 // ContentResolver dependency
 import com.google.mediapipe.formats.proto.LandmarkProto.Landmark;
 import com.google.mediapipe.formats.proto.LandmarkProto.NormalizedLandmark;
 import com.google.mediapipe.solutioncore.CameraInput;
 import com.google.mediapipe.solutioncore.SolutionGlSurfaceView;
-import com.google.mediapipe.solutioncore.VideoInput;
 import com.google.mediapipe.solutions.hands.HandLandmark;
 import com.google.mediapipe.solutions.hands.Hands;
 import com.google.mediapipe.solutions.hands.HandsOptions;
 import com.google.mediapipe.solutions.hands.HandsResult;
-import java.io.IOException;
-import java.io.InputStream;
 
 public class HomeFragment extends Fragment {
     private static String TAG = "HomeFragment" ;
@@ -56,8 +44,6 @@ public class HomeFragment extends Fragment {
     private static final boolean RUN_ON_GPU = true;
     private enum InputSource {
         UNKNOWN,
-        IMAGE,
-        VIDEO,
         CAMERA,
     }
     private InputSource inputSource = InputSource.UNKNOWN;
@@ -65,12 +51,12 @@ public class HomeFragment extends Fragment {
     // Image demo UI and image loader components.
     private HandsResultImageView imageView;
     // Video demo UI and video loader components.
-    private VideoInput videoInput;
     private ActivityResultLauncher<Intent> videoGetter;
     // Live camera demo UI and camera components.
     private CameraInput cameraInput;
 
     private SolutionGlSurfaceView<HandsResult> glSurfaceView;
+    private Button button;
 
     public HomeFragment(){
         Log.i(TAG,"Instantiated new "+this.getClass());
@@ -83,24 +69,36 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        button = binding.cameraButton;
+        checkPermission();
 
-        imageView = new HandsResultImageView(this.getContext());
-        setupLiveDemoUiComponents();
-
-        if (ContextCompat.checkSelfPermission(HomeFragment.this.getContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
-            requestPermissions(new String[]{Manifest.permission.CAMERA}, CAMERA_CODE);
-        }
         return root;
     }
 
+    public void checkPermission(){
+        if (ContextCompat.checkSelfPermission(HomeFragment.this.getContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+            binding.camera.setText("This feature needs to use the camera. Please allow us to have access to your camera.");
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, CAMERA_CODE);
+            button.setOnClickListener(new OnClickListener() {
+                public void onClick(View v) {
+                    requestPermissions(new String[] { Manifest.permission.CAMERA }, CAMERA_CODE);
+                }
+            });
+        } else {
+            imageView = new HandsResultImageView(this.getContext());
+            setupLiveDemoUiComponents();
+        }
+    }
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == CAMERA_CODE) {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(HomeFragment.this.getContext(), "camera permission granted", Toast.LENGTH_LONG).show();
+                Toast.makeText(HomeFragment.this.getContext(), "Camera Permission Granted", Toast.LENGTH_LONG).show();
+                imageView = new HandsResultImageView(this.getContext());
+                setupLiveDemoUiComponents();
             } else {
-                Toast.makeText(HomeFragment.this.getContext(), "camera permission denied", Toast.LENGTH_LONG).show();
+                Toast.makeText(HomeFragment.this.getContext(), "Camera Permission Denied", Toast.LENGTH_LONG).show();
             }
         }
     }
@@ -112,62 +110,18 @@ public class HomeFragment extends Fragment {
     @Override
     public void onPause() {
         super.onPause();
-        if (inputSource == InputSource.CAMERA) {
-            glSurfaceView.setVisibility(View.GONE);
-            cameraInput.close();
-        } else if (inputSource == InputSource.VIDEO) {
-            videoInput.pause();
-        }
+        glSurfaceView.setVisibility(View.GONE);
+        cameraInput.close();
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        if (inputSource == InputSource.CAMERA) {
-            // Restarts the camera and the opengl surface rendering.
-            cameraInput = new CameraInput(this.getActivity());
-            cameraInput.setNewFrameListener(textureFrame -> hands.send(textureFrame));
-            glSurfaceView.post(this::startCamera);
-            glSurfaceView.setVisibility(View.VISIBLE);
-        } else if (inputSource == InputSource.VIDEO) {
-            videoInput.resume();
-        }
-    }
-    private Bitmap rotateBitmap(Bitmap inputBitmap, InputStream imageData) throws IOException {
-        int orientation =
-                new ExifInterface(imageData)
-                        .getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
-        if (orientation == ExifInterface.ORIENTATION_NORMAL) {
-            return inputBitmap;
-        }
-        Matrix matrix = new Matrix();
-        switch (orientation) {
-            case ExifInterface.ORIENTATION_ROTATE_90:
-                matrix.postRotate(90);
-                break;
-            case ExifInterface.ORIENTATION_ROTATE_180:
-                matrix.postRotate(180);
-                break;
-            case ExifInterface.ORIENTATION_ROTATE_270:
-                matrix.postRotate(270);
-                break;
-            default:
-                matrix.postRotate(0);
-        }
-        return Bitmap.createBitmap(
-                inputBitmap, 0, 0, inputBitmap.getWidth(), inputBitmap.getHeight(), matrix, true);
-    }
-
-    private Bitmap downscaleBitmap(Bitmap originalBitmap) {
-        double aspectRatio = (double) originalBitmap.getWidth() / originalBitmap.getHeight();
-        int width = imageView.getWidth();
-        int height = imageView.getHeight();
-        if (((double) imageView.getWidth() / imageView.getHeight()) > aspectRatio) {
-            width = (int) (height * aspectRatio);
-        } else {
-            height = (int) (width / aspectRatio);
-        }
-        return Bitmap.createScaledBitmap(originalBitmap, width, height, false);
+        // Restarts the camera and the opengl surface rendering.
+        cameraInput = new CameraInput(this.getActivity());
+        cameraInput.setNewFrameListener(textureFrame -> hands.send(textureFrame));
+        glSurfaceView.post(this::startCamera);
+        glSurfaceView.setVisibility(View.VISIBLE);
     }
 
 
@@ -191,13 +145,9 @@ public class HomeFragment extends Fragment {
                                 .build());
         hands.setErrorListener((message, e) -> Log.e(TAG, "MediaPipe Hands error:" + message));
 
-        if (inputSource == InputSource.CAMERA) {
             cameraInput = new CameraInput(this.getActivity());
             cameraInput.setNewFrameListener(textureFrame -> hands.send(textureFrame));
-        } else if (inputSource == InputSource.VIDEO) {
-            videoInput = new VideoInput(this.getActivity());
-            videoInput.setNewFrameListener(textureFrame -> hands.send(textureFrame));
-        }
+
 
         // Initializes a new Gl surface view with a user-defined HandsResultGlRenderer.
         glSurfaceView =
@@ -233,16 +183,13 @@ public class HomeFragment extends Fragment {
                 CameraInput.CameraFacing.BACK,
                 glSurfaceView.getWidth(),
                 glSurfaceView.getHeight());
+        Toast.makeText(HomeFragment.this.getContext(), "Scanning", Toast.LENGTH_SHORT).show();
     }
 
     private void stopCurrentPipeline() {
         if (cameraInput != null) {
             cameraInput.setNewFrameListener(null);
             cameraInput.close();
-        }
-        if (videoInput != null) {
-            videoInput.setNewFrameListener(null);
-            videoInput.close();
         }
         if (glSurfaceView != null) {
             glSurfaceView.setVisibility(View.GONE);
